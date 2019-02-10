26aa8db7c262ad5abeb090d409ebc9e7e39fb6f6
unknown <igomez81@gmail.com>
2019-02-10 11:26:46 +0100

    Commit inicial

--- src/Controller/DeportesController.php
+++ src/Controller/DeportesController.php
@@ -3,18 +3,29 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 class DeportesController {
-  /**
-   * @Route("/deportes")
-   */
-  public function inicio() {
-      return new Response('Mi página de deportes!');
-  }
-  /**
-  * @Route("/deportes/primer-articulo")
-  */
-  public function mostrar() {
-     return new Response('Mi primera ruta Primer Artículo!');
-  }
+/**
+* @Route(
+*     "/deportes/{_locale}/{fecha}/{seccion}/{equipo}/{pagina}",
+*     defaults={"slug": "1","_format":"html","pagina":"1"},
+*     requirements={
+*         "_locale": "es|en",
+*         "_format": "html|json|xml",
+*         "fecha": "[\d+]{8}",
+*         "pagina"="\d+"
+*     }
+* )
+*/
+public function rutaAvanzadaListado($_locale, $fecha, $seccion, $equipo, $pagina) {
+ return new Response(sprintf(
+     'Listado de noticias  en idioma=%s,
+      fecha=%s,deporte=%s,equipo=%s, página=%s ',
+     $_locale, $fecha, $seccion, $equipo, $pagina));
+}
+
+
+
+
+
 }
 
 
