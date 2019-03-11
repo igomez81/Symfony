22dcdae53f2ddfed413631c21f0b93cc96766da7
unknown <igomez81@gmail.com>
2019-03-11 16:53:56 +0100

    Commit inicial

--- 09.02.Translations_Practice/src/Controller/DemoController.php
+++ 09.02.Translations_Practice/src/Controller/DemoController.php
@@ -1,19 +1,32 @@
 <?php
-// src/controller/DemoController.php
+//src/Controller/DemoController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Translation\TranslatorInterface;
 
 class DemoController extends AbstractController
 {
    /**
     * @Route("/demo", name="demo")
     */
-   public function index()
+   public function index(TranslatorInterface $translator)
    {
+       $translated = $translator->trans ('Hello');
+       dump($translated);
+       $name = 'Manolo';
+       $count = 2;
+       $translated = $translator->trans (
+           'Hello %name%',
+           [ '%name%' => $name ]
+       );
+       dump($translated);
        return $this->render('demo/index.html.twig', [
            'controller_name' => 'DemoController',
+           'name'=> $name,
+           'count' => $count
        ]);
    }
 }
